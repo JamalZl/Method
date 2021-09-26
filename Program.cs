477f999644e463fe89ebf31acc431b6895e00e80
Camal <90691266+JamalZl@users.noreply.github.com>
2021-09-26 20:58:22 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ededvemassiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of the array : ");
+            string n = Console.ReadLine();
+            int neded = int.Parse(n);
+            int[] arr = new int[neded];
+            int i;
+            Console.Write(" Enter the elements of an array: \n");
+            for (i = 0; i < arr.Length; i++)
+            {
+                Console.Write("{0} : ", i);
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+                Console.WriteLine("Given number");
+                string m = Console.ReadLine();
+                int meded = int.Parse(m);
+                Numberandarray(arr, meded);
+            static void Numberandarray(int[] arr, int meded)
+            {
+                int newnum=0;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] == meded)
+                    {
+                        newnum=1;
+                        Console.WriteLine("Given number is in the array");
+                        break;
+                    }
+                }
+                    if (newnum == 0)
+                    {
+                    Console.WriteLine("Given number isn't in the array");
+                    }
+            }
+        }
+    }
+}
+
